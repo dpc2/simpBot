e249ac8300b034d90cd9b05f63a7e5cd9aa96687
dpc2 <dpculler91@gmail.com>
2023-01-23 23:22:46 -0700

    Adding scraper code from Self Taught chapter 20

--- bot.py
+++ bot.py
@@ -1,4 +1,7 @@
 from mastodon import Mastodon
+import urllib.request
+from bs4 import BeautifulSoup
+
 
 #   Set up Mastodon
 mastodon = Mastodon(
@@ -6,4 +9,40 @@ mastodon = Mastodon(
     api_base_url = 'https://botsin.space/'
 )
 
-mastodon.status_post("hello world!")
+
+class Scraper:
+
+    # __init__ method takes a url to scrape as
+    # a parameter.
+    def __init__(self, site):
+        self.site = site
+
+    def scrape(self):
+        # urlopen() makes a request and returns
+        # a Response object. read() is used to
+        # return the HTML from the Return object.
+        r = urllib.request.urlopen(self.site)
+        html = r.read()
+
+        parser = 'html.parser'
+        sp = BeautifulSoup(html, parser)
+
+        # find_all returns an interable containing
+        # the tag objects it found
+        for tag in sp.find_all('img'):
+            # We get the 'href' tags by calling
+            # the get method and passing 'href'
+            url = tag.get('img')
+            if url is None:
+                continue
+            if 'img' in url:
+                print('\n' + url)
+
+
+simps = 'https://www.reddit.com/r/simpsonsshitposting/'
+print(Scraper(simps).scrape())
+
+
+
+
+#mastodon.status_post("hello world!")
